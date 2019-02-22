5c2d8a89adf5647d110d0c1d55c32bd31456acc4
goTenna <gotennaqa@gmail.com>
2019-02-22 10:21:57 -0500

    Initial Commit

---
+++ First_App_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First_App_Test
+//
+//  Created by GoTennaAutomation on 2/22/19.
+//  Copyright © 2019 GoTennaAutomation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ First_App_TestTests/First_App_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_App_TestTests.swift
+//  First_App_TestTests
+//
+//  Created by GoTennaAutomation on 2/22/19.
+//  Copyright © 2019 GoTennaAutomation. All rights reserved.
+//
+
+import XCTest
+@testable import First_App_Test
+
+class First_App_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
