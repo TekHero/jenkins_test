bd0d76f3c10ddcf7897b1552c41ffef367df8e46
goTenna <gotennaqa@gmail.com>
2019-02-22 11:51:24 -0500

    Second commit

--- First_App_Test/ViewController.swift
+++ First_App_Test/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myLabel.text = "Hello World!"
+        myLabel.textColor = UIColor.cyan
     }
 
 }
